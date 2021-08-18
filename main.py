efe4a144c91d9fb1dbb1cc03126d36c5258df5c1
Gokhan YILDIZ <gokhan7098@gmail.com>
2021-08-18 17:58:15 +0300

    master

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from flask import Flask,render_template,request,redirect,send_file
 from pytube import YouTube
-from flask import Flask,render_template
 import time
 
 
@@ -9,52 +9,51 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/music")
-def music():
-    return render_template('music.html')
+@app.route("/download",methods=['POST'])
+def download():
+    if request.method == 'POST':
+        selection = request.form
+        yt = YouTube(selection['url'])
+        if selection['type'] == 'music':
+            download_path = yt.streams.get_by_itag(140).download()
+            fname = download_path.split('//')[-1]
+            return send_file(fname, as_attachment=True)
+            #return render_template('download.html',url = yt.streams.get_by_itag(140).url)
+        elif selection['type'] == 'video':
+            download_path = yt.streams.get_highest_resolution().download()
+            fname = download_path.split('//')[-1]
+            return send_file(fname, as_attachment=True)
+            #return render_template('download.html',url = yt.streams.get_highest_resolution().url)
+        else:
+            return render_template('404.html'),404
+        
+        '''try:
+            yt = YouTube(selection['url'])
+            if selection['type'] == 'music':
+                yt.streams.get_by_itag(140).download()
+                path = '/'.yt.title.replace('.','') + '.mp4'
+                send_file(path, as_attachment=True)
+                return render_template('download.html',url = yt.streams.get_by_itag(140).url)
+            elif selection['type'] == 'video':
+                yt.streams.get_highest_resolution().download()
+                path = yt.title.replace('.','') + '.mp4'
+                send_file(path, as_attachment=True)
+                return render_template('download.html',url = yt.streams.get_highest_resolution().url)
+            else:
+                return render_template('404.html'),404
+        except:
+            return render_template('404.html'),404'''
+    else:
+        return render_template('404.html'),404
 
-@app.route("/video")
-def video():
-    return render_template('video.html')
 
 @app.errorhandler(404)
 def not_found(e):
     return render_template('404.html'),404
 
-
 if __name__ == "__main__":
     app.run()
 
-
-'''
-val = input('Video indirmek için "1", Müzik indirmek için "2":')
-choice = ''
-if val == "1":
-    print('Video indirmek için yönlendiriliyorsunuz.')
-    choice = 'V'
-elif val == "2":
-    print('Müzik indirmek için yönlendiriliyorsunuz.')
-    choice = 'M'
-else:
-    print('Geçersiz İşlem!')
-
-
-url = input('İndirmek istediğiniz video linkini yazınız:')
-try:
-    yt = YouTube(url)
-except:
-  print("Hata!")
-
-if choice == 'V':
-    print('İndiriliyor Lütfen Bekleyin!')
-    yt.streams.get_highest_resolution().download()
-elif choice == 'M':
-    print('İndiriliyor Lütfen Bekleyin!')
-    yt.streams.get_by_itag(140).download()
-else:
-    print('Bişey Oldu!')
-
-print('İşlem Tamam!')'''
 
 
 
