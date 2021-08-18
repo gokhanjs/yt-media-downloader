6c625309bc1bff0b353aac465b5278135facbb78
Gokhan YILDIZ <gokhan7098@gmail.com>
2021-08-18 18:02:16 +0300

    master

--- main.py
+++ main.py
@@ -13,36 +13,22 @@ def home():
 def download():
     if request.method == 'POST':
         selection = request.form
-        yt = YouTube(selection['url'])
-        if selection['type'] == 'music':
-            download_path = yt.streams.get_by_itag(140).download()
-            fname = download_path.split('//')[-1]
-            return send_file(fname, as_attachment=True)
-            #return render_template('download.html',url = yt.streams.get_by_itag(140).url)
-        elif selection['type'] == 'video':
-            download_path = yt.streams.get_highest_resolution().download()
-            fname = download_path.split('//')[-1]
-            return send_file(fname, as_attachment=True)
-            #return render_template('download.html',url = yt.streams.get_highest_resolution().url)
-        else:
-            return render_template('404.html'),404
-        
-        '''try:
+        try:
             yt = YouTube(selection['url'])
             if selection['type'] == 'music':
-                yt.streams.get_by_itag(140).download()
-                path = '/'.yt.title.replace('.','') + '.mp4'
-                send_file(path, as_attachment=True)
-                return render_template('download.html',url = yt.streams.get_by_itag(140).url)
+                download_path = yt.streams.get_by_itag(140).download()
+                fname = download_path.split('//')[-1]
+                return send_file(fname, as_attachment=True)
+                #return render_template('download.html',url = yt.streams.get_by_itag(140).url)
             elif selection['type'] == 'video':
-                yt.streams.get_highest_resolution().download()
-                path = yt.title.replace('.','') + '.mp4'
-                send_file(path, as_attachment=True)
-                return render_template('download.html',url = yt.streams.get_highest_resolution().url)
+                download_path = yt.streams.get_highest_resolution().download()
+                fname = download_path.split('//')[-1]
+                return send_file(fname, as_attachment=True)
+            #return render_template('download.html',url = yt.streams.get_highest_resolution().url)
             else:
                 return render_template('404.html'),404
         except:
-            return render_template('404.html'),404'''
+            return render_template('404.html'),404
     else:
         return render_template('404.html'),404
 
